681d696269739187b8894f454908f0f27fac6fb7
Nicola De Filippo <nicola@nicoladefilippo.it>
2022-02-14 09:14:20 +0100

    Initial Commit

---
+++ AppNicolaBrutta/AppNicolaBruttaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppNicolaBruttaApp.swift
+//  AppNicolaBrutta
+//
+//  Created by nicola on 14/02/22.
+//
+
+import SwiftUI
+
+@main
+struct AppNicolaBruttaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
